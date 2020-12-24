002fb0e61f14ac8910987b1967bba1d840069ba4
Omar Gamaleldeen | Ejada Systems Ltd <ogameleldeen@ejada.com>
2020-12-24 17:57:25 +0300

    The model of show

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -58,7 +58,16 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
 
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer,db.ForeignKey(Venue.id))
+    venue_name = db.Column(db.String(120),db.ForeignKey(Venue.name))
+    artist_id = db.Column(db.Integer,db.ForeignKey(Artist.id))
+    artist_name =  db.Column(db.String(120),db.ForeignKey(Artist.name))
+    artist_image_link = db.Column(db.String(500),db.ForeignKey(Artist.image_link))
+    start_time = db.Column(db.DateTime)
 
 db.create_all()
 db.session.commit()
